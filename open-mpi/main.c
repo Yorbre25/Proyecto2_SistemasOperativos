efe53f7c0945950410bb0018d86a3fe1365eef70
JonDGS <jongs.estudiantec.cr>
2023-11-05 20:19:39 -0600

    Added decrypting code
    Also added an example of the encrypted and decrypted text

---
+++ open-mpi/main.c
@@ -0,0 +1,60 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SHIFT 7
+
+void decrypt(char *buffer, int size) {
+  for(int i=0; i<size; i++) {
+    if(buffer[i] >= 'A' && buffer[i] <= 'Z') {
+      buffer[i] = ((buffer[i] - 'A' - SHIFT) % 26) + 'A';
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+
+  MPI_Init(&argc, &argv);
+  
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  char *filename = argv[1];
+  
+  FILE *fp = fopen(filename, "r");
+  fseek(fp, 0L, SEEK_END);
+  long file_size = ftell(fp);
+
+  int chunk_size = file_size / size;
+  
+  fseek(fp, rank * chunk_size, SEEK_SET);
+
+  char *buffer = malloc(chunk_size);
+  fread(buffer, chunk_size, 1, fp);
+  fclose(fp);
+
+  decrypt(buffer, chunk_size);
+
+  char *full_data;
+  if (rank == 0) {
+    full_data = malloc(file_size);
+  }
+
+  MPI_Gather(buffer, chunk_size, MPI_CHAR, full_data, chunk_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    FILE *out = fopen("decrypted.txt", "w");
+    fwrite(full_data, file_size, 1, out);
+    fclose(out);
+  }
+
+  free(buffer);
+  if (rank == 0) {
+    free(full_data);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
